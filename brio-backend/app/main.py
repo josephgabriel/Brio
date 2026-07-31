c764460e634066bd3b4a67e282cbe06dab7c4a4e
José Gabriel Soares dos Santos <josgabrielsoares825@gmail.com>
2026-07-31 14:08:34 -0300

    ...

--- brio-backend/app/main.py
+++ brio-backend/app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://brio-rho.vercel.app",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
